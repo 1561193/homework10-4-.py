7654276b332a43730f82962b1bb74805ef605a69
1561193 <164223652+1561193@users.noreply.github.com>
2024-04-01 14:07:00 +0300

    Add files via upload
    Нет уверенности что сделал правильно

---
+++ homework10(4).py
@@ -0,0 +1,15 @@
+car_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in car_:
+    name = 'Я езжу на автомобиле марки '
+    print(name,i)
+
+cars_count = 0
+for i in range(cars_count, 100, 10):
+    print(i)
+
+
+
+
+
+
+
